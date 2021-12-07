a8a3c0fb2c24396735e6e39d9dc2e99a073aa439
JapanCodeman <ryansurdick@MacBook-Pro.mshome.net>
2021-12-07 17:49:57 +0900

    Search bar functionality incorporated for both home and results components

--- src/components/home.js
+++ src/components/home.js
@@ -4,16 +4,27 @@ import Logo from './logo';
 import SearchBar from './searchbar';
 import RecentPosts from './recentposts';
 
-export default class Home extends Component {
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class Home extends Component {
+  
+  handleSearchBarSubmit(query) {
+    this.props.fetchPostsWithQuery(query);
+    this.props.history.push('/results');
+  }
+
   render() {
     return (
       <div className='app'>
         <div>
           <Logo />
-          <SearchBar />
+          <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
           <RecentPosts />
         </div>
       </div>
     );
   }
 }
+
+export default connect(null, actions)(Home)
--- src/components/results.js
+++ src/components/results.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import Logo from './logo';
 import SearchBar from './searchbar';
+import ResultsPosts from './resultsPosts';
 
-export default class Results extends Component {
+
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
+class Results extends Component {
+
+  handleSearchBarSubmit(query){
+    this.props.fetchPostsWithQuery(query);
+  }
   render () {
     return (
       <div>
         <Logo size={55}/>
-        <SearchBar />
+        <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
+        <ResultsPosts />
       </div>
     );
   }
 }
+
+export default connect(null, actions)(Results);
