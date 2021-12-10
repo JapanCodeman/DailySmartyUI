d14323efa542d6dcf31d3cb264f5fdaa9ebfbfc6
JapanCodeman <ryansurdick@MacBook-Pro.mshome.net>
2021-12-10 17:11:29 +0900

    added lots of styles to the results page, added a no content <div> to display when a result has no links, and basically prepared app for deployment

--- src/components/post.js
+++ src/components/post.js
@@ -22,6 +22,25 @@ export default class Post extends Component {
     return topics;
   }
 
+  getNameForPostLink(str) {
+    var n = str.lastIndexOf('/');
+    var link = str.substring(n + 1, str. length);
+
+    if((n + 1) == str.length) {
+      link=str.slice(0, n);
+      n = link.lastIndexOf('/');
+      link = str.substring(n + 1, str.length -1)
+    }
+
+    if (link.includes('.html')) {
+      link = link.substring(0, link.length - 5);
+    }
+    if (link.includes('.htm')) {
+      link = link.substring(0, link.length - 4);
+    }
+    return link;
+  }
+
   renderLinks() {
     let links = this.props.post_links.map((post_link, index) => {
       return (
@@ -29,11 +48,14 @@ export default class Post extends Component {
           <div className="post-link__box"></div>
 
           <div className="post-link__link">
-            <a href={post_link.link_url}>Useful link #{index + 1}</a>
+            <a href={post_link.link_url}>{this.getNameForPostLink(post_link.link_url)}</a>
           </div>
         </div>
       );
     });
+    if(links == 0) {
+      return <div className="no-content">No Post Links</div>
+    }
     return links;
   }
 
@@ -48,11 +70,10 @@ export default class Post extends Component {
       );
     } else if (this.props.type == "result") {
       return (
-        <li
-          className="result-post"
+        <li className="result-post"
           onMouseEnter={() => this.setState({ height: 70 })}
           onMouseLeave={() => this.setState({ height: 0 })}
-        >
+          >
           <div className="result-post__topics">{this.renderTopics()}</div>
 
           <div className="result-post__title">
